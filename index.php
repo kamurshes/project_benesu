d684443956dd1334a8ed8238df81a660555218f1
kamurshes <kamurshes@menz-do.club>
2019-11-23 15:16:20 +0900

    QuickReplyの実装

--- index.php
+++ index.php
@@ -265,7 +265,7 @@ function BeaconProtocol($bot,$event)
 	$_MSG=new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($MSG);
 	$bot->replyMessage($event->getReplyToken(), ($_MSG));
 	error_log(print_r($_MSG,true));
-	$QR=new LINE\LINEBot\TemplateActionBuilder("テスト1","テスト2","テスト3");
+	$QR=new \LINE\LINEBot\TemplateActionBuilder("テスト1","テスト2","テスト3");
 	error_log(print_r($QR,true));
 	//$bot->replyMessage($event->getReplyToken(),(new LINE\LINEBot\QuickReplyBuilder\QuickReplyButtonBuilder(new LINE\LINEBot\TemplateActionBuilder("テスト1","テスト2","テスト3"),null)));
 
